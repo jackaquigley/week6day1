67f7205b00a9f89bd3c3698c3478eb7efbf0fa80
Jack Quigley <jackaquigley@gmail.com>
2020-04-20 19:44:08 +0100

    webpage now shows the name of the item object on the main page

--- src/app.js
+++ src/app.js
@@ -3,5 +3,12 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
+    data: {
+      items: [
+        {name: "Feed Son", itemPriority: false},
+        {name: "Go To Shop", itemPriority: true}
+      ],
+      newItem: ""
+    }
   })
 });
